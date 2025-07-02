deebb4a85674e06d218eb0825d403188ea65f15f
ItaloAurelioSenai <0001115971@senaimgaluno.com.br>
2025-07-02 19:21:27 -0300

    feature: criação da página inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="w-screen h-screen flex align-middle justify-center bg-gray-200">
+      <div className="g-20">
+        <h1 className="text-[36px] text-gray-800 font-bold">Prova Versionamento - RPV Senai Cataguases</h1>
+        <p className="text-[24px] text-slate-500">Ítalo Aurélio de Paula Vieira</p>
+        <p className="text-[24px] text-slate-400">Cataguases, 02 de Julho de 2025</p>
+      </div>
+    </div>
   )
 }
