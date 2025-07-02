952ceb860269ebadb4bc3fe59147153cc7686920
ItaloAurelioSenai <0001115971@senaimgaluno.com.br>
2025-07-02 19:38:24 -0300

    feature: criação da página de contato

---
+++ src/app/contato/index.jsx
@@ -0,0 +1,11 @@
+export default function Contato() {
+  return (
+    <div className="w-screen h-screen flex align-middle justify-center bg-gray-200">
+      <div className="g-20">
+        <h1 className="text-[36px] text-slate-800">Seja bem-vindo à página de Contato</h1>
+        <p className="text-[24px] text-slate-500">Ítalo Aurélio de Paula Vieira</p>
+        <p className="text-[24px] text-slate-400">Cataguases, 02 de Julho de 2025</p>
+      </div>
+    </div>
+  )
+}
